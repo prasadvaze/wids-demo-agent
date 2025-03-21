ff388ca2c9bccb37a1d397905480a7285d0b716c
Michelle Rojas <michelle.rojas@graphika.com>
2025-03-20 21:07:26 -0400

    feat: add tortoise orm, add first api test

--- src/wids/data-agent/api/app/__init__.py
+++ src/wids/data-agent/api/app/__init__.py
@@ -1,12 +1,27 @@
+import os
+
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+
+from app.routes import ping
 
 
-def create_app() -> FastAPI:
+def create_application() -> FastAPI:
     app = FastAPI(
         title="Secret Agent API",
         description="API for demoing the multi-agent killer",
         version="0.1.0",
     )
+
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=False,
+        add_exception_handlers=True,
+    )
+
+    app.include_router(ping.router)
 
     @app.get("/")
     async def root():
--- src/wids/data-agent/api/app/main.py
+++ src/wids/data-agent/api/app/main.py
@@ -1,21 +1,10 @@
 import logging
 
-from app import create_app
-from app.config import Settings, get_settings
-from fastapi import Depends
+from app import create_application
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-app = create_app()
-
-
-@app.get("/ping")
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-        "ping": "pong!",
-        "environment": settings.environment,
-        "testing": settings.testing,
-    }
+app = create_application()
